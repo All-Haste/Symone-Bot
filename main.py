7aa3f4737cd58361311565481bb2762a2aa7ee96
Eric Ziegler <eric.ziegler@protonmail.com>
2021-08-13 17:06:20 -0600

    formatting fixes, grab GM id from env

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import os
 from flask import jsonify
 from slack.signature import SignatureVerifier
 
-GAME_MASTER = "U72P1S26N"
+# This is the ID of the GM user in slack
+# TODO: create a proper user permissions system.
+GAME_MASTER = os.environ["GAME_MASTER"]
 
 
 def verify_signature(request):
     request.get_data()  # Decodes received requests into request.data
 
-    verifier = SignatureVerifier(os.environ['SLACK_SECRET'])
+    verifier = SignatureVerifier(os.environ["SLACK_SECRET"])
 
     if not verifier.is_valid_request(request.data, request.headers):
-        raise ValueError('Invalid request/credentials.')
+        raise ValueError("Invalid request/credentials.")
 
 
 def symone_bot(request):
-    if request.method != 'POST':
-        return 'Only POST requests are accepted', 405
+    if request.method != "POST":
+        return "Only POST requests are accepted", 405
 
     verify_signature(request)
 
@@ -27,8 +29,8 @@ def symone_bot(request):
 
 def symone_message():
     message = {
-        'response_type': 'in_channel',
-        'text': 'Hello',
+        "response_type": "in_channel",
+        "text": "Hello",
     }
 
     return message
