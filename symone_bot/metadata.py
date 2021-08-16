99d63ce804bbb25de8b0cc2109c41db5e1a180cd
Eric Ziegler <eric.ziegler@protonmail.com>
2021-08-15 21:29:38 -0600

    tests for metadata

--- symone_bot/metadata.py
+++ symone_bot/metadata.py
@@ -2,6 +2,6 @@ class QueryMetaData:
     """Stores metadata about the incoming query such as user-id, headers, etc."""
 
     def __init__(self, user_id: str):
-        if user_id is None:
+        if not isinstance(user_id, str):
             raise AttributeError("'user_id' cannot be None.")
         self.user_id = user_id
