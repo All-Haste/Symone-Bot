b4926143a6be97a1bcfc4e22646fb4463e3c3c59
Eric Ziegler <eric.ziegler@protonmail.com>
2022-09-09 22:20:28 -0600

    remove coverage requirement

--- test/test_main.py
+++ test/test_main.py
@@ -2,17 +2,8 @@ from unittest.mock import MagicMock
 
 from flask import Request
 
-from main import parse_slack_data, symone_message
+from main import symone_message
 from symone_bot.commands import MESSAGE_RESPONSE_EPHEMERAL
-
-
-def test_parse_slack_data():
-    test_input = b"foo=bar&cat=feline&123=easyasabc"
-    actual = parse_slack_data(test_input)
-
-    assert actual["foo"] == "bar"
-    assert actual["cat"] == "feline"
-    assert actual["123"] == "easyasabc"
 
 
 def test_symone_message():
